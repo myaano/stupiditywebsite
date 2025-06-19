55096b497cc9bdc12d53bcce1217057d58ae4f96
myaano <minguri73@gmail.com>
2025-06-19 11:01:00 +0800

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,12 @@ const instrumentSans = Instrument_Sans({
   variable: '--font-instrument-sans',
 });
 
+import { Urbanist } from "next/font/google";
+const urbanist = Urbanist({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-urbanist',
+});
 
 
 export default function Home() {
@@ -30,38 +36,42 @@ export default function Home() {
       <Header />
       {/* END OF HEADER*/}
       {/*Landing Page Intro*/}
-      <div className="flex">
-        <div className="bg-red-500">
-          <Image
-            src="/20240410_111015.webp"
-            alt="Landing Image"
-            width={620}
-            fill
-            height={200}
-            quality={100}
+        <div className={`${urbanist.variable} flex`}>
+          <div className="relative w-full md:max-w-[620px] md:h-[525px] lg:w-[1024px]">
+            <Image
+              src="/20240410_111015.webp"
+              alt="Fuji"
+              fill
+              style={{ objectFit: "cover" }}
+              quality={100}
+              />
+              
+              {/*Landing Page Text*/}
+              <div className="absolute text-white font-urbanist text-7xl mt-19 ml-10 drop-shadow-2xl">Travel with ease</div>
+              <div className="absolute text-white font-urbanist text-7xl mt-41 ml-11">in</div>
+              <div className="absolute text-white font-urbanist text-7xl mt-62 ml-10">Japan</div>
+              <div className="absolute text-white font-urbanist text-lg mt-92 ml-10">Explore Japan and see the Eye Catching Tokyo Skytree,</div>
+              <div className="absolute text-white font-urbanist text-lg mt-97 ml-10">Ice Caps of Mt. Fuji, Temples of Kyoto,</div>
+              <div className="absolute text-white font-urbanist text-lg mt-102 ml-10">to the Snowy Land of Hokkaido, and to the Beaches of</div>
+              <div className="absolute text-white font-urbanist text-lg mt-107 ml-10">Okinawa we will guide you there!</div>
+              {/*Landing Page Text End*/}
+
+          </div>
+          <div className="relative w-full max-w-[148px] h-[525px]">
+            <Image
+              src="/20230609_173348.jpg"
+              alt="Skytree"
+              fill
+              style={{ objectFit: "cover" }}
+              quality={100}
             />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          </div>
         </div>
-        <div className="bg-green-500">awdw</div>
-      </div>
+
+        <div className="flex flex-col">
+          <div className="">adwwadadw</div>
+          <div>awdawdawda</div>
+        </div>
     </main>
     </>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,8 +16,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className={`${instrumentSans.variable} bg-white md:px-8 lg:px-16 py-[25px] border-b border-b-black shadow-xl`}>
-      <div className="flex justify-between">
-        <Link href="/" className="font-instrument-sans lg:text-xl">Home</Link>
+      <div className="flex justify-between px-10">
+        <Link href="/" className="font-instrument-sans lg:text-xl ">Home</Link>
         <div className="font-instrument-sans">Dark Mode???</div>
       </div>
     </div>
