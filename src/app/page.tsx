f5101240ae34927d46568df433c377b31c6d00e0
myaano <minguri73@gmail.com>
2025-06-24 19:25:43 +0800

    yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,14 +36,20 @@ export default function Home() {
       
       <div className={`${urbanist.variable}`}>
         <div className="pt-18">
-          <div className="font-urbanist text-white">
-            <span className="text-3xl absolute pl-7  text-center sm:top-45 sm:text-4xl sm:pl-14 md:top-53 md:text-5xl md:pl-18">Journey with Ease</span>
+          <div className="font-urbanist text-white text-3xl absolute pl-5 top-27 sm:top-45 sm:text-4xl sm:pl-14 md:top-53 md:text-5xl md:pl-18">
+            <p className="">Journey with Ease</p>
+            <p className="">Across Japan</p>
+          </div>
+          <div className="font-urbanist text-white text-[10px] absolute top-65 pl-7 ">
+            <p className="">Travel across Japan and see the Ice Caps of Mt.Fuji,</p>
+            <p className="">the Eye Catching Tokyo Skytree,</p>
+            
           </div>
           <Image
           src="/20240410_11101222.jpg"
           alt="fuji"
           width={768}
-          height={600}
+          height={400}
           className="w-full h-auto"
           />
         </div>
