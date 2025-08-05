624b66ced6bbf87bda363bbf846c8d423434efd6
myaano <minguri73@gmail.com>
2025-08-05 10:32:24 +0800

    chaged smooth scroll from lenis to gsap, and added parallax to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import { Manrope } from "next/font/google";
+
 const manrope = Manrope({
   subsets: ["latin"],
   weight: ["200", "400"],
@@ -6,17 +7,17 @@ const manrope = Manrope({
 });
 export default function Footer() {
   return (
-    <div className={`${manrope.variable} flex flex-col w-full h-[50vh] bg-[rgb(17,17,17)] p-10`}>
-        <div className="flex flex-col flex-1 text-white font-manrope text-3xl ">
-            <p>Note!</p>
-            <p className="text-[15px] pt-3">I credit everyone who owns every single picture I have used in this website. This website that I had created is for my WebDev skills only. </p>
+    <div className={`${manrope.variable} grid grid-cols-2 gap-3 w-full h-[100vh] bg-[#1a1a1a] p-5 lg:p-7 font-manrope overflow-hidden`}>
+      <div className="flex-1 flex justify-center items-center  text-neutral-300 text-3xl font-bold">
+        <p data-speed="0.5" >Credits to everyone who owns the photos used in this website.</p>
+      </div>
+      <div className="flex-1 grid grid-rows-2 justify-center items-center  text-neutral-300 text-2xl font-bold" >
+        <p data-speed="0.5">Dev: myaano</p>
+        <div className="flex flex-col justify-start items-start" data-speed="0.5" >
+          <p>Email:</p>
+          <p className="text-[14px]" >minguri73@gmail.com</p>
         </div>
-        <div className="flex flex-col flex-1 justify-start items-center text-white font-manrope">
-            <p>Developer:</p>
-            <p className="pt-2">myaano</p>
-            <p className="pt-2">Contact:</p>
-            <p className="pt-2">minguri73@gmail.com</p>
-        </div>
+      </div>
     </div>
   );
 }
